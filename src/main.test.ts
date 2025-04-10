a7c51bf10734679646d1a5d9140466c23e29a23f
Valeriia <valeriia.chyrko@oa.edu.ua>
2025-04-10 14:48:34 +0300

    Add testing configuration with Vitest and Testing Library.

---
+++ src/main.test.ts
@@ -0,0 +1,19 @@
+﻿import { screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+
+// Імітуємо HTML, який рендериться в main.ts
+const setup = () => {
+    document.body.innerHTML = `
+    <div id="app">
+      <h1>Vite + TypeScript</h1>
+    </div>
+  `;
+};
+
+describe('Main', () => {
+    it('renders headline', () => {
+        setup();
+        const headline = screen.getByText(/Vite \+ TypeScript/i);
+        expect(headline).toBeInTheDocument();
+    });
+});
