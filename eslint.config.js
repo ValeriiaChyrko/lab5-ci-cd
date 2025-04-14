8a93ae8e8f2a96337c9a8eb7fd9d114024022a20
Valeriia <valeriia.chyrko@oa.edu.ua>
2025-04-14 11:56:04 +0300

    Updated eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,35 @@
 import { defineConfig } from "eslint/config";
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import pluginReact from "eslint-plugin-react";
+import globals from "globals";
 
-
-
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    rules: {
-      "react/react-in-jsx-scope": "off",
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
-      "no-console": "error",
+export default defineConfig({
+  files: ["**/*.{ts,tsx,js,jsx}"],
+  languageOptions: {
+    parser: tsParser,
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    globals: {
+      ...globals.browser,
+      ...globals.node,
     },
   },
-]);
+  plugins: {
+    "@typescript-eslint": tseslint,
+    react: pluginReact,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "no-console": "warn",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+  }
+});
